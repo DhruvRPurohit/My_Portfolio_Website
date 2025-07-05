5866ccec84ef97bf11b66b026ebfd382cd7ca867
Dhruv <dhruvr.purohit>
2025-07-05 19:10:36 +0530

    [add]:add the header

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+
+const Home = () => {
+  return (
+    <main className='flex min-h-screen flex-col bg-[#121212] mx-auto px-12 py-4'>
+        <HeroSection />
+    </main>
+  )
+}
+
+export default Home
